553079d27b246c01415e9445fcbb0b5fa3ec6100
Fernando <lucvs177@gmail.com>
2022-09-21 00:55:48 -0300

    se agrego useeffect y se obtiene los personajes

--- mod3-challenge01/src/components/Character.jsx
+++ mod3-challenge01/src/components/Character.jsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import { getCharacters } from "../service";
 
 const CharacterList = () => {
     const [characterList, setCharacterList] = useState([])
     const [isLoading, setIsLoading] = useState(false)
     const [image, setImage] = useState('')
 
-
+    useEffect(()=>{
+        setIsLoading(true);
+        getCharacters()
+            .then((data) => setCharacterList(data.results))
+            .catch((err) => console.log(err))
+            .finally(() => setIsLoading(false))
+    },[])
     
 
 return(
     <div className='page'>
-    
+        {isLoading && <span className="loading-text">Loading</span>}
     <div>
         <img src={image} alt="No hay imagen" />
     </div>
